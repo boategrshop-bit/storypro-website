ccaec6c61afbf79ed141df0ebdd26df061ecbc05
boategrshop-bit <boategrshop@gmail.com>
2026-06-14 01:57:45 +0700

    Fix Apps Script URL typo (PuIg not PulG)

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const upload = multer({
 });
 
 // ---------- EMAIL ----------
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx7q8DMEA_gZqVICFFpLdSw23PulgLtcztcXsr1DlMILjDZcanRtspfi0TELoczs3ZK/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx7q8DMEA_gZqVICFFpLdSw23PuIgLtcztcXsr1DlMILjDZcanRtspfi0TELoczs3ZK/exec';
 
 async function sendEmail(data) {
   try {
